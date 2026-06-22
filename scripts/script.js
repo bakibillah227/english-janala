a7d4529ab93bfbd4d1502f6ab31d5e0a6923b7b4
bakibillah227 <bakibillah227@gmail.com>
2026-06-22 11:13:30 +0600

    display details using DaisyUI Modal

--- scripts/script.js
+++ scripts/script.js
@@ -22,6 +22,49 @@ const loadLevelWord = (id) => {
     });
 };
 
+const loadWordDetail = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetails(details.data);
+};
+
+const displayWordDetails = (word) => {
+  console.log(word);
+  const detailsBox = document.getElementById("details-container");
+  detailsBox.innerHTML = `
+  <div>
+            <h2 class="text-2xl font-bold">
+              ${word.word} (<i class="fa-solid fa-microphone-lines"></i> :${word.pronunciation})
+            </h2>
+          </div>
+          <div>
+            <h2 class="font-bold">
+              Meaning
+            </h2>
+            <p>${word.meaning}</p>
+          </div>
+          <div>
+            <h2 class="font-bold">
+              Example
+            </h2>
+            <p>${word.sentence}</p>
+          </div>
+          <div>
+            <h2 class="font-bold">
+              Synonym
+            </h2>
+            <span class="btn">Syn1</span>
+            <span class="btn">Syn1</span>
+            <span class="btn">Syn1</span>
+          </div>
+  `;
+
+  document.getElementById("word_modal").showModal();
+
+}
+
 const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
@@ -53,7 +96,7 @@ const displayLevelWord = (words) => {
         <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি "} / ${word.pronunciation ? word.pronunciation : "Pronounciation পাওয়া যায়নি"}"</div>
 
         <div class="flex justify-between items-center">
-          <button onclick="my_modal_5.showModal()" class="btn hover:btn-bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+          <button onclick="loadWordDetail(${word.id})" class="btn hover:btn-bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
           <button class="btn hover:btn-bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
         </div>
       </div>
@@ -81,4 +124,5 @@ const displayLessons = (lessons) => {
     levelContainer.append(btnDiv);
   }
 };
+
 loadLessons();
